851628af3c3c68421cb9e8f82870ec05b31f775c
Richard Durham <richard_durham@me.com>
2016-01-09 15:51:42 +0000

    feat: Begin Punchstarter project

---
+++ punchstarter/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from flask.ext.script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+    # render_template automaticially looks for templates in the templates folder
