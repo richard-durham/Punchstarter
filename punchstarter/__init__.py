fb034b3d86de9ce6d2dd1dfb3c240a544408ea00
Richard Durham <richard_durham@me.com>
2016-01-10 16:09:03 +0000

    feat: Add settings for db and link to Flask

--- punchstarter/__init__.py
+++ punchstarter/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import sqlalchemy
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 
 app = Flask(__name__)
+app.config.from_object('punchstarter.default_settings') #let Flask know about the settings file
 manager = Manager(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app)
+manager.add_command('db', MigrateCommand)
 
 @app.route("/")
 def hello():
